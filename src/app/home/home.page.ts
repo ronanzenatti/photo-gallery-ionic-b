f2ea3dedc200ad47c1ea2bd464249d5ae4e949e5
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2022-10-07 20:10:04 -0300

    Adicionou a Camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public photoService: PhotoService) {}
 
-  constructor() {}
-
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
